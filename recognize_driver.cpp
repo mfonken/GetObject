289be9975715569ab291b1bd7c618c99fb3b56df
Matthew Fonken <matthewfonken@letu.edu>
2015-01-18 15:30:50 -0600

    Commit

---
+++ recognize_driver.cpp
@@ -0,0 +1,9 @@
+//
+//  recognize_driver.cpp
+//  
+//
+//  Created by Matthew Fonken on 1/18/15.
+//
+//
+
+#include "recognize_driver.h"
---
+++ recognize_driver.h
@@ -0,0 +1,14 @@
+//
+//  recognize_driver.h
+//  
+//
+//  Created by Matthew Fonken on 1/18/15.
+//
+//
+
+#ifndef ____recognize_driver__
+#define ____recognize_driver__
+
+#include <stdio.h>
+
+#endif /* defined(____recognize_driver__) */
