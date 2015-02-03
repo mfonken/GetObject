6db73fbd6bea0302628eae2d566491cc4552793b
mfonken <mistertux27@gmail.com>
2015-02-03 17:32:57 -0600

    commit

--- Scan.cpp
+++ Scan.cpp
@@ -7,8 +7,8 @@
 //
 
 #include <stdio.h>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
+#include "opencv/imgproc/include/opencv2/imgproc/imgproc.hpp"
+#include "opencv/highgui/include/opencv2/highgui/highgui.hpp"
 
 using namespace cv;
 
