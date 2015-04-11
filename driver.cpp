59a7b9338475ea4ef46c96b6af60fccc81951536
mfonken <mistertux27@gmail.com>
2015-04-11 14:53:26 -0500

    4/10 -

--- driver.cpp
+++ driver.cpp
@@ -11,20 +11,20 @@
 #include "CameraTest.cpp"
 
 int main(int argc, const char * argv[]) {
-    int duration = 30;
+    int count = 30;
     int thresh = 50;
-    if (argc > 1) duration = atoi(argv[1]);
+    if (argc > 1) count = atoi(argv[1]);
     if (argc > 2) thresh = atoi(argv[2]);
-    std::cout << "Running GetObject for " << duration << " seconds at threshold " << thresh << std::endl;
+    std::cout << "Running GetObject for " << count << " seconds at threshold " << thresh << std::endl;
     Object object;
     //object.getAverage();
     char list;
-    for (int i = 0; i < duration; i++)
+    for (int i = 0; i < count; i++)
         object.captureImage();
-    //std::cout << object.getObject(duration, list, matList[0], thresh) << std::endl;
+    //std::cout << object.getObject(count, list, matList[0], thresh) << std::endl;
     std::cout << object.scanImageForCharacter(list,thresh) << std::endl;
     //Tester tester;
-    //std::cout << tester.display(duration) << std::endl;
+    //std::cout << tester.display(count) << std::endl;
     std::cout << "ended" << std::endl;
     return 0;
 }
