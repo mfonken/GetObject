c8cad36ba655b9c1cc893ef471fa8a2691ba5852
mfonken <mistertux27@gmail.com>
2015-04-12 20:04:51 -0500

    4/12

--- GetObject.cpp
+++ GetObject.cpp
@@ -34,7 +34,7 @@ public:
     Object();
     void getAverage();
     bool captureImage();
-    bool getObject(int duration, char& characterList, Mat& matList, int thresh);
+    bool getObject(int duration, char& characterList, int thresh);
     bool scanImageForCharacter(char& characterList, int thresh);
     int getDiff(Mat&, int&, int&,int*);
 };
@@ -100,7 +100,7 @@ void Object::getAverage() {
     avg[0] = 0;                                      //...and divide by length to get average
     avg[1] = 0;
     avg[2] = 0;
-    /*
+    
     for (int i = offset; i < samp+offset; i++) {        //Samples on a diagonal from top-left corner plus offset down size of "samp"
         Vec3b tmp_color = capture.at<Vec3b>(Point(offset,i));   //Add up each color channel...
         avg[0] += (int)tmp_color[2];
@@ -110,15 +110,15 @@ void Object::getAverage() {
     avg[0] /= samp;                                      //...and divide by length to get average
     avg[1] /= samp;
     avg[2] /= samp;
-     */
-    //std::cout << "Background is (" << avg[0] << ", " << avg[1] << ", " << avg[2] << ")" << std::endl;
+     
+    std::cout << "Background is (" << avg[0] << ", " << avg[1] << ", " << avg[2] << ")" << std::endl;
     gettimeofday(&te, NULL);
     duration = (te.tv_sec - tb.tv_sec) * 1000;      // sec to ms
     duration += (te.tv_usec - tb.tv_usec) / 1000.0;
     //std::cout << duration << "ms" << std::endl;
 }
 
-bool Object::getObject(int duration, char& characterList, Mat& matList, int thresh) {
+bool Object::getObject(int duration, char& characterList, int thresh) {
     diff_thresh = thresh;
     //std::cout << "Checking Object for " << duration << "s:\n";
     timeval tb, te, tt;
@@ -140,7 +140,7 @@ bool Object::getObject(int duration, char& characterList, Mat& matList, int thre
         while (y < video.size().height) {                   //Dual loop scans row from top to bottom, setting pixels to BGR to B&W based off distance from avgs
             while (x < video.size().width) {                //white if similar, black if unique
                 //video.at<Vec3b>(y,x) = (getDiff(video, x, y, avg) > diff_thresh) ? Vec3b(1,1,1):Vec3b(0,0,0);
-                video.at<Vec3b>(y,x) = (getDiff(video, x, y, avg) < diff_thresh) ? Vec3b(0,0,0):Vec3b(255,255,255);
+                video.at<Vec3b>(y,x) = (getDiff(video, x, y, avg) > diff_thresh) ? Vec3b(0,0,0):Vec3b(255,255,255);
                 //video.at<Vec3b>(y,x) = getDiff(video, x, y, avg);
                 x++;
             }
@@ -149,12 +149,12 @@ bool Object::getObject(int duration, char& characterList, Mat& matList, int thre
         }   //(Yes, for loops would have been easier)
         
         // **********Display Test Start********** //
-        
+        /*
         Mat sizeMat(30,30,  CV_8UC3), temp;
         resize(video, sizeMat, sizeMat.size(), INTER_LINEAR);
         cv::cvtColor(sizeMat, temp, CV_BGR2GRAY);
         std::cout << "Frame: " << std::endl << temp << std::endl;
-        
+        */
 	// **********Display Test End********** //
         x = video.size().width/2;                                              //Reset to first pixel again
         y = video.size().height/2;
@@ -191,7 +191,7 @@ bool Object::getObject(int duration, char& characterList, Mat& matList, int thre
                         resize(tmp, object, object.size(), INTER_LINEAR);                           //...then resize it linearly to correct size (100x100)
                         //cv::cvtColor(object,object, CV_BGR2BW);
                         CheckObject checker;
-			//std::cout << object << std::endl;
+                        //std::cout << object << std::endl;
                         c = checker.checkChar(object, (float)object_height/(float)object_width);                                                      //Finally check it using "checkChar"
                         //c = absDiff(object);
                         
@@ -213,7 +213,7 @@ bool Object::getObject(int duration, char& characterList, Mat& matList, int thre
             }
         }
     end_loop:
-        //imshow("Filtered", video);                      //End of loop, below is where the string of found chars and "character.txt" are managed
+        imshow("Filtered", video);                      //End of loop, below is where the string of found chars and "character.txt" are managed
         
         if (c != last_char && c != '~') {                           //If a different character is found (and not a '~' local null), c and last_char while be different...
             bool exists = false;                                    //reset temporary flag
@@ -355,17 +355,18 @@ bool Object::scanImageForCharacter(char& characterList, int thresh) {
 int Object::getDiff(Mat &video, int &x, int &y, int* background) { //As described above, this is a literal distance formula, given r, g, and b are used as base references
     
     Vec3b tmp_color = video.at<cv::Vec3b>(Point(x,y));
-    //int tmp_diff = abs(background[2] - tmp_color[0]);
-    //return tmp_diff;
+    int tmp_diff = abs(background[2] - tmp_color[0]) + abs(background[1] - tmp_color[1]) + abs(background[0] - tmp_color[2]);
+    //std::cout << "Diff: " << tmp_diff << std::endl;
+    return tmp_diff;
    
     //std::cout << "O:" << (int)tmp_color[0] << std::endl;
     //int tmp_total = tmp_color[0] + tmp_color[1] + tmp_color[2] + 1;
-    //double scaling_factor = 3*255/tmp_total;
+    ////double scaling_factor = 3*255/tmp_total;
     int greyness = abs(tmp_color[0]-tmp_color[2])+abs(tmp_color[2]-tmp_color[1])+abs(tmp_color[1]-tmp_color[0]);
     //abs(255 - tmp_color[0]*scaling_factor) + abs(255 - tmp_color[1]*scaling_factor) + abs(255 - tmp_color[2]*scaling_factor);
     //std::cout << "O:" << greyness << std::endl;
-    return greyness;
+    //return greyness;
 }
 int abs(int n) {
-   return (n > 0 ? n:-n);
+   return (n > 0 ? n:0);
 }
